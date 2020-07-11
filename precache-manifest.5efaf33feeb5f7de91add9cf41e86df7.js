641c1ba0b448c6a1f0dcc9e38a16484c56629b40
Jesse VanSlyke <jesse.j.vanslyke@gmail.com>
2020-07-11 01:14:05 -0400

    Updates

---
+++ precache-manifest.5efaf33feeb5f7de91add9cf41e86df7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b815e10905727781bb83191235b98e91",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c53b6e973ee15d27483b",
+    "url": "/react-portfolio/static/css/main.26c38953.chunk.css"
+  },
+  {
+    "revision": "d1af5232cd4af00a31ff",
+    "url": "/react-portfolio/static/js/2.3ef1f71c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.3ef1f71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53b6e973ee15d27483b",
+    "url": "/react-portfolio/static/js/main.155f244d.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "b1bb7b22c250195dff4a9ffd0c7559b2",
+    "url": "/react-portfolio/static/media/burger-app.b1bb7b22.PNG"
+  },
+  {
+    "revision": "b7bbef1d7f72c9c7f5ec4fbbce386ca5",
+    "url": "/react-portfolio/static/media/day-planner-app.b7bbef1d.PNG"
+  }
+]);
